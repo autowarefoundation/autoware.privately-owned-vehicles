86d757d4ec5db44889753d10932226a501782436
Sohel Mahmud <sohel.digitalhive@gmail.com>
2025-01-08 15:51:37 -0500

    Finished trajector draw functions

--- PathDet/create_path/ROADWork/process_roadwork.py
+++ PathDet/create_path/ROADWork/process_roadwork.py
@@ -49,7 +49,7 @@ def create_output_subdirs(output_dir):
     """
     Create subdirectories for the output directory
     """
-    subdirs_list = ["image", "segmentation", "visualization"]
+    subdirs_list = ["image", "visualization", "segmentation"]
     output_subdirs = []
 
     for subdir in subdirs_list:
@@ -63,7 +63,7 @@ def check_directory_exists(directory_path: str):
     """Check if a directory exists; if not, create it."""
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
-        logger.info(f"Directory created: {directory_path} !")
+        logger.info(f"Directory created: {directory_path}")
     else:
         print(f"Directory '{directory_path}' already exists.")
 
@@ -94,71 +94,63 @@ def merge_json_files(json_dir):
     
     return merged_data
 
-def draw_trajectory_line_points(image_path, trajectory, output_dir):
-    # Split the trajectory into x and y coordinates
-    x_coords = np.array([point[0] for point in trajectory])
-    y_coords = np.array([point[1] for point in trajectory])
-
-    # Perform cubic spline interpolation
-    cs_x = CubicSpline(np.arange(len(x_coords)), x_coords, bc_type="natural")
-    cs_y = CubicSpline(np.arange(len(y_coords)), y_coords, bc_type="natural")
-
-    # Generate new interpolated points
-    new_x = np.linspace(0, len(x_coords)-1, num=500)
-    new_y = cs_y(new_x)  # Get y coordinates using the cubic spline
-
-    # Load your image where the trajectory should be drawn
-    with Image.open(image_path) as img:
-        # Create a Draw object to draw on the image
-        draw = ImageDraw.Draw(img)
-   
-
-    # Draw the interpolated trajectory on the image
-    for i in range(len(new_x) - 1):
-        start_point = (int(cs_x(new_x[i])), int(new_y[i]))
-        end_point = (int(cs_x(new_x[i + 1])), int(new_y[i + 1]))
-        draw.line([start_point, end_point], fill='red', width=3)  # Draw the line
-
-    # Optional: Draw original points in blue
-    for x, y in zip(x_coords, y_coords):
-        draw.ellipse([x - 5, y - 5, x + 5, y + 5], fill='blue', outline='blue')  # Draw original points
-
-    # Save the modified image with the overlay
-    img.save()
-
-def draw_trajectory_line(image_path, trajectory, output_path):
+def draw_trajectory_line(image_path, image_id, trajectory, output_subdirs):
     """
-    Draw Trajectory on the image
+    Draw the trajectory line both on the image and the mask, and save
     """
+
+    # Open the image and create a draw object
     with Image.open(image_path) as img:
-        draw = ImageDraw.Draw(img)
-    
-    # Draw the drivable line
-    for i in range(len(trajectory) - 1):
-        # Set the start and end point
-        start_point = trajectory[i]
-        end_point = trajectory[i + 1]
+        img_width, img_height = img.size
+        img_draw = ImageDraw.Draw(img)
+
+        # Create a new image name
+        new_img = f"{image_id}.png"
+
+        # Create a new image with black background
+        mask = Image.new("L", (img_width, img_height), 0)
+
+        # Create a draw object
+        mask_draw = ImageDraw.Draw(mask)
+
+        # Split the trajectory into x and y coordinates
+        x_coords = np.array([point[0] for point in trajectory])
+        y_coords = np.array([point[1] for point in trajectory])
+
+        # Perform cubic spline interpolation
+        cs_x = CubicSpline(np.arange(len(x_coords)), x_coords, bc_type="natural")
+        cs_y = CubicSpline(np.arange(len(y_coords)), y_coords, bc_type="natural")
+
+        # Generate new interpolated points
+        new_x = np.linspace(0, len(x_coords)-1, num=500)
+        new_y = cs_y(new_x)  # Get y coordinates using the cubic spline
+
+        # Draw the interpolated trajectory on the image
+        for i in range(len(new_x) - 1):
+            # Start and end point
+            start_point = (int(cs_x(new_x[i])), int(new_y[i]))
+            end_point = (int(cs_x(new_x[i + 1])), int(new_y[i + 1]))
+
+            # Draw the line
+            img_draw.line([start_point, end_point], fill='yellow', width=5)
+            mask_draw.line([start_point, end_point], fill=255, width=5)
+
+        # # Optional: Draw original points in blue
+        # for x, y in zip(x_coords, y_coords):
+        #     mask_draw.ellipse([x - 5, y - 5, x + 5, y + 5], fill='red', outline='blue')  # Draw original points
+
+        ##### OVERLAY IMAGE #####
+        # Save or show the modified image with the overlay
+        img.save(os.path.join(output_subdirs[1], new_img))
+        # Log the result for trajectory line overlay
+        logger.info(f"Overlay Trajectory Image generated in: {output_subdirs[1]}")
+
+        ##### MASK #####
+        # Save the mask with trajectory line
+        mask.save(os.path.join(output_subdirs[2], new_img))
+        # Log the result for trajectory line mask
+        logger.info(f"Trajectory Mask generated in: {output_subdirs[2]}")
         
-        # Draw the line
-        draw.line([start_point, end_point], fill="yellow", width=10)
-    
-    img.save(output_path)
-
-def draw_trajectory_points(image_path, trajectory, output_path):
-    """
-    Draw Trajectory on the image
-    """
-    radius = 5
-    point_color = (255, 0, 0)
-    with Image.open(image_path) as img:
-        draw = ImageDraw.Draw(img)
-    
-    for point in trajectory:
-        x, y = point["x"], point["y"]
-        draw.ellipse([x-radius, y-radius, x+radius, y+radius], fill=point_color)
-    
-    img.save(output_path)
-
 def create_drivable_path_json(output_dir, json_data):
     pass
 
@@ -181,20 +173,26 @@ def create_trajectory_mask(image_shape, mask_name, trajectory, output_dir,  lane
     # Save the mask in output directory
     mask.save(os.path.join(output_dir, mask_name))
 
-def convert_jpg2png(image_path, output_dir):
+def convert_jpg2png(image_id, image_path, output_subdir):
     """
     Convert JPG image to PNG image
     """
-    with Image.open(image_path) as img:
-        # Get the Image ID and concat with PNG extention
-        new_img = f"{get_id(image_path)}.png"
-        
-        # Copy raw img and put it in raw dir.
-        img.save(os.path.join(output_dir, new_img))
-        
+    with Image.open(image_path) as image:
+        # Create a new image name
+        new_img = f"{image_id}.png"
+
+        # Save the image in PNG format
+        image.save(os.path.join(output_subdir, new_img))
+    
         # Log the result
         logger.info(f"Converted JPG to PNG image: {new_img}")
 
+def get_image_shape(image_path):
+    """
+    Get the shape of the image
+    """
+    with Image.open(image_path) as img:
+        return img.size
 
 def main(args):
 
@@ -203,35 +201,45 @@ def main(args):
     output_dir = args.output_dir
     draw_mode = args.draw_mode
 
-    #### STEP 01: Check and Create Output Directories
-    # Check Output directory exists
-    create_output_subdirs(output_dir)
+    #### STEP 01: Create subdirectories for the output directory
+    output_subdirs = create_output_subdirs(output_dir)
+    print(output_subdirs)
 
-    ### STEP 02: List all the images in the image directory
-    # List all the images in the image directory
-    img_list = os.listdir(image_dir)
-
-    #### STEP 03: Read all JSON files and create JSON data (list of dictionaries)
+    #### STEP 02: Read all JSON files and create JSON data (list of dictionaries)
     json_data = merge_json_files(json_dir)
     print(len(json_data))
 
-    #### STEP 04: Parse JSON data and create drivable path JSON file
-    #### STEP 04(a): Read Image
-    #### STEP 04(b): Read Trajectory and process the trajectory points
-    #### STEP 04(c): Normalize the trajectory points
-    #### STEP 04(d): Create drivable path JSON file
-    #### STEP 04(e): Draw Trajectory on the image
-    #### STEP 04(f): Create Trajectory Mask
+    ### STEP 04: Parse JSON data and create drivable path JSON file
+    ### STEP 04(a): Convert JPG to PNG format
+    ### STEP 04(b): Read Trajectory and process the trajectory points
+    ### STEP 04(c): Draw Trajectory line on the image and save
+    ### STEP 04(d): Create Trajectory Mask and save
+    ### STEP 04(e): Normalize the trajectory points
+    ### STEP 04(f): Create drivable path JSON file
+
     for i in json_data:
-        id = i["id"]
-        img_path = i["image"]
+        image_id = i["id"]
+        image_path = os.path.join(image_dir, i["image"])
+        print(image_path)
+
+        # Convert JPG to PNG and store in output directory
+        convert_jpg2png(image_id, image_path, output_subdirs[0])
         
-        # Fetche the trajectories and process them as tuples
+        # Fetch and process the trajectory points as tuples
         trajectory = process_trajectory(i["trajectory"])
 
-        # Normalize the trajectory points
-        normalized_trajectory = normalize_coords(trajectory, Image.open(img_path).size)
+        # Draw Trajectory line on the image and save
+        draw_trajectory_line(image_path, image_id, trajectory, output_subdirs)
 
+        # Get Image shape
+        image_shape = get_image_shape(image_path)
+
+        # Normalize the trajectory points
+        norm_trajectory = normalize_coords(trajectory, image_shape)
+
+        # Create drivable path JSON file
+
+        break
 
 
 
