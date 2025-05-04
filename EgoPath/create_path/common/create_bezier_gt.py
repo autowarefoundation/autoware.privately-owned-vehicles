2e4bba6a087d885a370cb7106be7fed989d3d789
TranHuuNhatHuy <huy9515@gmail.com>
2025-05-04 18:11:29 +0900

    done argparse

--- EgoPath/create_path/common/create_bezier_gt.py
+++ EgoPath/create_path/common/create_bezier_gt.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import cv2
 from PIL import Image
 import numpy as np
+import argparse
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(
@@ -20,4 +21,25 @@ from Models.data_utils.load_data_ego_path import (
 
 
 if (__name__ == "__main__"):
-    
+
+    # == Input args ==
+
+    parser = argparse.ArgumentParser(
+        description = "Process CurveLanes dataset - PathDet groundtruth generation"
+    )
+    parser.add_argument(
+        "-d", "--dataset_dir",
+        dest = "dataset_dir", 
+        type = str, 
+        help = f"Master dataset directory, should contain all 6 datasets: {VALID_DATASET_LIST}",
+        required = True
+    )
+    args = parser.parse_args()
+
+    # Parse dirs
+    dataset_dir = args.dataset_dir
+
+    # == Preprocess ==
+
+    list_subdirs = ["image", "segmentation", "visualization"]
+    json_path = "drivable_path.json"
