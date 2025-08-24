44d4b317b7543f8b38edf21067c66e1a775ccd95
TranHuuNhatHuy <huy9515@gmail.com>
2025-08-24 16:22:41 +0900

    format functions

--- EgoPath/create_path/OpenLane/process_openlane.py
+++ EgoPath/create_path/OpenLane/process_openlane.py
@@ -8,3 +8,28 @@ import math
 import warnings
 import numpy as np
 from PIL import Image, ImageDraw
+
+
+# ============================= Format functions ============================= #
+
+
+def round_line_floats(line, ndigits = 6):
+    line = list(line)
+    for i in range(len(line)):
+        line[i] = [
+            round(line[i][0], ndigits),
+            round(line[i][1], ndigits)
+        ]
+    line = tuple(line)
+    return line
+
+
+# Custom warning format
+def custom_warning_format(
+    message, 
+    category, filename, 
+    lineno, line = None
+):
+    return f"WARNING : {message}\n"
+
+warnings.formatwarning = custom_warning_format
