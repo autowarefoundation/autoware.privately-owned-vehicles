d4d798411a613d7e9e2fe89d1a9ed6b431e22e26
TranHuuNhatHuy <huy9515@gmail.com>
2025-08-24 19:29:43 +0900

    deadling with args

--- EgoPath/create_path/OpenLane/process_openlane.py
+++ EgoPath/create_path/OpenLane/process_openlane.py
@@ -67,4 +67,65 @@ if __name__ == "__main__":
     W = 1920
     H = 1280
 
+    # ============================== Parsing args ============================== #
+
+    parser = argparse.ArgumentParser(
+        description = "Process OpenLane dataset - groundtruth generation"
+    )
+    parser.add_argument(
+        "--dataset_dir", 
+        type = str, 
+        help = "OpenLane raw directory",
+        required = True
+    )
+    parser.add_argument(
+        "--output_dir", 
+        type = str, 
+        help = "Output directory",
+        required = True
+    )
+    # For debugging only
+    parser.add_argument(
+        "--early_stopping",
+        type = int,
+        help = "Num. files you wanna limit, instead of whole set.",
+        required = False
+    )
+
+    args = parser.parse_args()
+
+    # Parse dirs
+    dataset_dir = args.dataset_dir
+    output_dir = args.output_dir
+
+    # Parse early stopping
+    if (args.early_stopping):
+        print(f"Early stopping set, stopping after {args.early_stopping} files.")
+        early_stopping = args.early_stopping
+    else:
+        early_stopping = None
+
+    # Generate output structure
+    """
+    Due to the huge dataset size, and since we don't have to edit the raw images,
+    I have decided to not outputing the raw image files, but instead only the
+    visualizations and groundtruth JSON.
+
+    --output_dir
+        |----visualization
+        |----drivable_path.json
+
+    """
+
+    list_subdirs = ["visualization"]
+
+    if (os.path.exists(output_dir)):
+        warnings.warn(f"Output directory {output_dir} already exists. Purged")
+        shutil.rmtree(output_dir)
+
+    for subdir in list_subdirs:
+        subdir_path = os.path.join(output_dir, subdir)
+        if (not os.path.exists(subdir_path)):
+            os.makedirs(subdir_path, exist_ok = True)
+
     
