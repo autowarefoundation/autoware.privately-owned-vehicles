344012ac4b436dd94b5755e502466df9d6a500cf
JITERN <limjitern@gmail.com>
2025-08-24 23:28:28 +0800

    publish CARLA control commands
    Signed-off-by: JITERN <limjitern@gmail.com>

--- VisionPilot/ROS2/pure_pursuit/include/pure_pursuit_node.hpp
+++ VisionPilot/ROS2/pure_pursuit/include/pure_pursuit_node.hpp
@@ -1,14 +1,16 @@
 #pragma once
 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/float32_multi_array.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float32_multi_array.hpp>
 #include "pure_pursuit.hpp"
+#include <carla_msgs/msg/carla_ego_vehicle_control.hpp>
 
 class PurePursuitNode : public rclcpp::Node
 {
 public:
     PurePursuitNode(const rclcpp::NodeOptions &options);
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_;
+    rclcpp::Publisher<carla_msgs::msg::CarlaEgoVehicleControl>::SharedPtr steering_pub_;
     void computeSteering(const std_msgs::msg::Float32MultiArray::SharedPtr msg);
 private:
     PurePursuit pp;
--- VisionPilot/ROS2/pure_pursuit/src/pure_pursuit_node.cpp
+++ VisionPilot/ROS2/pure_pursuit/src/pure_pursuit_node.cpp
@@ -1,16 +1,18 @@
 #include "pure_pursuit_node.hpp"
 
-PurePursuitNode::PurePursuitNode(const rclcpp::NodeOptions &options) : Node("pure_pursuit_node", "/pure_pursuit", options), pp(2.7)
+PurePursuitNode::PurePursuitNode(const rclcpp::NodeOptions &options) : Node("pure_pursuit_node", "", options), pp(2.7)
 {
 
   this->set_parameter(rclcpp::Parameter("use_sim_time", true));
-  sub_ = this->create_subscription<std_msgs::msg::Float32MultiArray>("tracked_states", 10, std::bind(&PurePursuitNode::computeSteering, this, std::placeholders::_1));
+  sub_ = this->create_subscription<std_msgs::msg::Float32MultiArray>("/pathfinder/tracked_states", 10, std::bind(&PurePursuitNode::computeSteering, this, std::placeholders::_1));
+  steering_pub_ = this->create_publisher<carla_msgs::msg::CarlaEgoVehicleControl>("carla/hero/vehicle_control_cmd", 10);
   RCLCPP_INFO(this->get_logger(), "PurePursuit Node started");
 }
 
 void PurePursuitNode::computeSteering(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
 {
-  if (msg->data.size() < 13) {
+  if (msg->data.size() < 13)
+  {
     RCLCPP_WARN(this->get_logger(), "Received message with insufficient data size: %zu", msg->data.size());
     return;
   }
@@ -19,6 +21,16 @@ void PurePursuitNode::computeSteering(const std_msgs::msg::Float32MultiArray::Sh
   double curvature = msg->data[11];
   double forward_velocity = 10.0; // Placeholder for actual velocity input
   double steering_angle = pp.computeSteering(cte, yaw_error, curvature, forward_velocity);
+
+  auto control_msg = carla_msgs::msg::CarlaEgoVehicleControl();
+  control_msg.header.stamp = this->now();
+  control_msg.steer = steering_angle;// to normalized value between -1 and 1
+  control_msg.throttle = 0.5; // relate to velocity
+  control_msg.brake = 0.0;
+  control_msg.hand_brake = false;
+  control_msg.reverse = false;
+  control_msg.manual_gear_shift = false;
+  steering_pub_->publish(control_msg);
 
   RCLCPP_INFO(this->get_logger(), "Computed steering angle: %f", steering_angle);
 }
