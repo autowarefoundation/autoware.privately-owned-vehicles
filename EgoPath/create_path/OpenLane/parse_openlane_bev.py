f5339982baa0bad391e411345b5d5a61f6c2acbe
TranHuuNhatHuy <huy9515@gmail.com>
2025-09-07 20:29:17 +0900

    add format functions

--- EgoPath/create_path/OpenLane/parse_openlane_bev.py
+++ EgoPath/create_path/OpenLane/parse_openlane_bev.py
@@ -10,3 +10,26 @@ import numpy as np
 from PIL import Image, ImageDraw
 
 
+# ============================== Format functions ============================== #
+
+
+def round_line_floats(line, ndigits = 6):
+    line = list(line)
+    for i in range(len(line)):
+        line[i] = [
+            round(line[i][0], ndigits),
+            round(line[i][1], ndigits)
+        ]
+    line = tuple(line)
+    return line
+
+
+def custom_warning_format(message, category, filename, lineno, line = None):
+    return f"WARNING : {message}\n"
+
+warnings.formatwarning = custom_warning_format
+
+
+# ============================== Helper functions ============================== #
+
+
