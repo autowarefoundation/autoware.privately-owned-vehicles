2f6f4754219d9e89f1252ef54e6f6abb7c7bf7a0
TranHuuNhatHuy <huy9515@gmail.com>
2025-09-08 09:36:48 +0900

    add getLineAnchor aux func

--- EgoPath/create_path/OpenLane/parse_openlane_bev.py
+++ EgoPath/create_path/OpenLane/parse_openlane_bev.py
@@ -273,6 +273,33 @@ def interpLine(line: list, points_quota: int):
     return list(zip(x_new, y_new))
 
 
+def getLineAnchor(line, new_img_height):
+    """
+    Determine "anchor" point of a line.
+    """
+    (x2, y2) = line[0]
+    (x1, y1) = line[1]
+
+    for i in range(1, len(line) - 1, 1):
+        if (line[i][0] != x2) & (line[i][1] != y2):
+            (x1, y1) = line[i]
+            break
+
+    if (x1 == x2) or (y1 == y2):
+        if (x1 == x2):
+            error_lane = "Vertical"
+        elif (y1 == y2):
+            error_lane = "Horizontal"
+        warnings.warn(f"{error_lane} line detected: {line}, with these 2 anchors: ({x1}, {y1}), ({x2}, {y2}).")
+        return (x1, None, None)
+    
+    a = (y2 - y1) / (x2 - x1)
+    b = y1 - a * x1
+    x0 = (new_img_height - b) / a
+
+    return (x0, a, b)
+
+
 def polyfit_BEV(
     bev_line: list,
     order: int,
