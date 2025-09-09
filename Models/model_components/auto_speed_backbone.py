b20b8f82e27a236e6aa0891eae7a8daaf4d68e97
Atanasko Boris Mitrev <atanasko.mitrev@exo.mk>
2025-09-09 13:20:05 +0200

    Add AutoSpeed network, trainer, visualization code

---
+++ Models/model_components/auto_speed_backbone.py
@@ -0,0 +1,49 @@
+import torch
+from Models.model_components.auto_speed_layers import Conv
+from Models.model_components.auto_speed_layers import C3K2
+from Models.model_components.auto_speed_layers import SPPF
+from Models.model_components.auto_speed_layers import C2PSA
+from Models.model_components.auto_speed_layers import PSA
+from Models.model_components.auto_speed_layers import CSP
+from Models.model_components.auto_speed_layers import SPP
+
+
+class AutoSpeedBackbone(torch.nn.Module):
+    def __init__(self, width, depth, csp):
+        super().__init__()
+        self.p1 = []
+        self.p2 = []
+        self.p3 = []
+        self.p4 = []
+        self.p5 = []
+
+        # p1/2
+        self.p1.append(Conv(width[0], width[1], torch.nn.SiLU(), k=3, s=2, p=1))
+        # p2/4
+        self.p2.append(Conv(width[1], width[2], torch.nn.SiLU(), k=3, s=2, p=1))
+        self.p2.append(C3K2(width[2], width[3], depth[0], csp[0], r=4))
+        # p3/8
+        self.p3.append(Conv(width[3], width[3], torch.nn.SiLU(), k=3, s=2, p=1))
+        self.p3.append(C3K2(width[3], width[4], depth[1], csp[0], r=4))
+        # p4/16
+        self.p4.append(Conv(width[4], width[4], torch.nn.SiLU(), k=3, s=2, p=1))
+        self.p4.append(C3K2(width[4], width[4], depth[2], csp[1], r=2))
+        # p5/32
+        self.p5.append(Conv(width[4], width[5], torch.nn.SiLU(), k=3, s=2, p=1))
+        self.p5.append(C3K2(width[5], width[5], depth[3], csp[1], r=2))
+        self.p5.append(SPPF(width[5], width[5]))
+        self.p5.append(C2PSA(width[5], width[5]))
+
+        self.p1 = torch.nn.Sequential(*self.p1)
+        self.p2 = torch.nn.Sequential(*self.p2)
+        self.p3 = torch.nn.Sequential(*self.p3)
+        self.p4 = torch.nn.Sequential(*self.p4)
+        self.p5 = torch.nn.Sequential(*self.p5)
+
+    def forward(self, x):
+        p1 = self.p1(x)
+        p2 = self.p2(p1)
+        p3 = self.p3(p2)
+        p4 = self.p4(p3)
+        p5 = self.p5(p4)
+        return p3, p4, p5
