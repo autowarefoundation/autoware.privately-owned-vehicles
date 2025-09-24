abd2a552cb6a1681f9dcdea32e199ddcf714e4ab
ChenYing Kuo <evshary@gmail.com>
2025-09-24 16:32:22 +0800

    Align the latency calculation.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- VisionPilot/ROS2/models/include/run_model_node.hpp
+++ VisionPilot/ROS2/models/include/run_model_node.hpp
@@ -1,11 +1,14 @@
 #ifndef RUN_MODEL_NODE_HPP_
 #define RUN_MODEL_NODE_HPP_
 
+#include "../../common/include/fps_timer.hpp"
 #include "../../common/include/inference_backend_base.hpp"
 #include <rclcpp/rclcpp.hpp>
 #include <image_transport/image_transport.hpp>
 #include <memory>
 #include <chrono>
+
+#define BENCHMARK_OUTPUT_FREQUENCY 100
 
 namespace autoware_pov::vision
 {
@@ -29,12 +32,9 @@ private:
   // ROS
   image_transport::Subscriber sub_;
   image_transport::Publisher pub_;
-  
-  // Latency monitoring (like original AUTOSEG)
-  static constexpr size_t LATENCY_SAMPLE_INTERVAL = 100; // Log every 100 frames
-  size_t frame_count_ = 0;
-  bool measure_latency_ = false;
-  std::chrono::steady_clock::time_point inference_start_time_;
+
+  // Benchmark: Timer
+  FpsTimer timer_;
 };
 
 }  // namespace autoware_pov::vision
