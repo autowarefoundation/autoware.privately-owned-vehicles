2002524835d4179a828358b621a8257e2eec0906
JITERN <limjitern@gmail.com>
2025-10-08 21:34:34 +0800

    remove longitudinal PI control from steering node
    Signed-off-by: JITERN <limjitern@gmail.com>

--- VisionPilot/ROS2/steering_controller/src/steering_controller_node.cpp
+++ VisionPilot/ROS2/steering_controller/src/steering_controller_node.cpp
@@ -1,12 +1,10 @@
 #include "steering_controller_node.hpp"
 
-  // it is observed that turning radius increases with speed, with the same steering angle command
-  // velocity m/s, turning radius m
-  // <=2.5 , 3.0
-  // 5.0   , 3.5
-  // 7.5   , 4
-
-// TODO: separate longitudinal PI controller core cpp implementation from ROS2 node
+// it is observed that turning radius increases with speed, with the same steering angle command
+// velocity m/s, turning radius m
+// <=2.5 , 3.0
+// 5.0   , 3.5
+// 7.5   , 4
 
 SteeringControllerNode::SteeringControllerNode(const rclcpp::NodeOptions &options) : Node("steering_controller_node", "", options),
                                                                                     sc(2.85, 2.0, 2.8, 1.0, 4.0)
@@ -26,40 +24,7 @@ SteeringControllerNode::SteeringControllerNode(const rclcpp::NodeOptions &option
 
 void SteeringControllerNode::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
-  double vx = msg->twist.twist.linear.x;
-  double vy = msg->twist.twist.linear.y;
-  forward_velocity_ = vx; // convert to km/h
-
-  // ------------------------------
-  // PI Controller for throttle
-  // ------------------------------
-  double v_ref = 22.22; // [m/s]
-  double error = v_ref - vx;
-
-  // Integrator update (with anti-windup)
-  integral_error_ += error * 0.01;
-  integral_error_ = std::clamp(integral_error_, -5.0, 5.0); // limit integral windup
-
-  double kp = 1.0;
-  double ki = 0.07;
-  double u = kp * error + ki * integral_error_;
-
-  double throttle_cmd = (u > 0.0) ? std::clamp(u, 0.0, 1.0) : 0.0;
-  double brake_cmd = (u < 0.0) ? std::clamp(-u, 0.0, 1.0) : 0.0;
-
-  auto control_msg = carla_msgs::msg::CarlaEgoVehicleControl();
-  control_msg.header.stamp = this->now();
-  // TODO: ackermann model with 48deg and 70deg, turning radius seems to be f(steering_angle, speed)
-  // control_msg.steer = std::clamp((180.0 / M_PI) * (steering_angle_ / 49.0), -1.0, 1.0); // 49deg is for low speeds 0.5m/s
-
-  control_msg.steer = std::clamp((180.0 / M_PI) * (steering_angle_ / 49.0), -1.0, 1.0);
-
-  control_msg.throttle = throttle_cmd;
-  control_msg.brake = brake_cmd;
-  control_msg.hand_brake = false;
-  control_msg.reverse = false;
-  control_msg.manual_gear_shift = false;
-  steering_pub_->publish(control_msg);
+  forward_velocity_ = msg->twist.twist.linear.x; // in m/s
 }
 
 void SteeringControllerNode::stateCallback(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
@@ -73,6 +38,18 @@ void SteeringControllerNode::stateCallback(const std_msgs::msg::Float32MultiArra
   yaw_error_ = msg->data[7];
   curvature_ = msg->data[11];
   steering_angle_ = sc.computeSteering(cte_, yaw_error_, forward_velocity_, curvature_);
+
+  auto control_msg = carla_msgs::msg::CarlaEgoVehicleControl();
+  control_msg.header.stamp = this->now();
+  // TODO: ackermann model with 48deg and 70deg, turning radius seems to be f(steering_angle, speed)
+  // control_msg.steer = std::clamp((180.0 / M_PI) * (steering_angle_ / 49.0), -1.0, 1.0); // 49deg is for low speeds 0.5m/s
+  control_msg.steer = std::clamp((180.0 / M_PI) * (steering_angle_ / 49.0), -1.0, 1.0);
+  control_msg.throttle = 0.6;
+  control_msg.brake = 0.0;
+  control_msg.hand_brake = false;
+  control_msg.reverse = false;
+  control_msg.manual_gear_shift = false;
+  steering_pub_->publish(control_msg);
 }
 
 int main(int argc, char *argv[])
