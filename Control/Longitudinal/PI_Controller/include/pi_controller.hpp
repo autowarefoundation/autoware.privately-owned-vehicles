e2513105a62943c06ec7af16c2ec78f123ad7621
JITERN <limjitern@gmail.com>
2025-10-08 20:54:22 +0800

    PI controller cpp
    Signed-off-by: JITERN <limjitern@gmail.com>

---
+++ Control/Longitudinal/PI_Controller/include/pi_controller.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+class PI_Controller
+{
+public:
+    PI_Controller(double K_p, double K_i);
+    double computeEffort(double current_speed_, double target_speed_);
+
+private:
+    double K_p, K_i, integral_error;
+};
