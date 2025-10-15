bb68377d696d77441c718bb943deefc93b9fb10f
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2025-10-15 13:18:22 +0300

    adding back segmentation branch

--- Models/model_components/auto_steer_network.py
+++ Models/model_components/auto_steer_network.py
@@ -2,6 +2,8 @@ from .backbone import Backbone
 from .backbone_feature_fusion import BackboneFeatureFusion
 from .auto_steer_context import AutoSteerContext
 from .auto_steer_head import AutoSteerHead
+from .ego_path_neck import EgoPathNeck
+from .ego_path_head import EgoPathHead
 
 
 import torch.nn as nn
@@ -21,12 +23,20 @@ class AutoSteerNetwork(nn.Module):
 
         # AutoSteer Prediction Head
         self.AutoSteerHead = AutoSteerHead()
+
+        # EgoPath Neck
+        self.EgopathNeck = EgoPathNeck()
+
+        # EgoPath Head
+        self.EgoPathHead = EgoPathHead()
     
 
     def forward(self, image):
         features = self.BEVBackbone(image)
         fused_features = self.BackboneFeatureFusion(features)
         context = self.AutoSteerContext(fused_features)
+        neck = self.EgopathNeck(context, features)
         path_prediction = self.AutoSteerHead(context)
+        seg_prediction = self.EgoPathHead(neck, features)
         
-        return path_prediction
+        return path_prediction, seg_prediction
