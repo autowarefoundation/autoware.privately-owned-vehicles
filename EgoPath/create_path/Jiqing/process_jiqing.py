bfef822b4197d152d40db5cc3bc6346691dd0cc6
TranHuuNhatHuy <huy9515@gmail.com>
2025-10-27 12:06:32 +0900

    type declarations

--- EgoPath/create_path/Jiqing/process_jiqing.py
+++ EgoPath/create_path/Jiqing/process_jiqing.py
@@ -9,3 +9,11 @@ import numpy as np
 from tqdm import tqdm
 from typing import Any
 from PIL import Image, ImageDraw, ImageFont
+
+
+# ============================= Format functions ============================= #
+
+
+PointCoords = tuple[float, float]
+ImagePointCoords = tuple[int, int]
+Line = list[PointCoords] | list[ImagePointCoords]
