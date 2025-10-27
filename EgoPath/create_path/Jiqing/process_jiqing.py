2d8c49ebf52575f79e9bf4a0d27c7de3fa643986
TranHuuNhatHuy <huy9515@gmail.com>
2025-10-27 12:06:55 +0900

    add round line to float function

--- EgoPath/create_path/Jiqing/process_jiqing.py
+++ EgoPath/create_path/Jiqing/process_jiqing.py
@@ -17,3 +17,22 @@ from PIL import Image, ImageDraw, ImageFont
 PointCoords = tuple[float, float]
 ImagePointCoords = tuple[int, int]
 Line = list[PointCoords] | list[ImagePointCoords]
+
+
+def round_line_floats(
+    line: Line, 
+    ndigits: int = 3
+):
+    """
+    Round the coordinates of a line to a specified number of decimal places.
+    """
+
+    line = list(line)
+    for i in range(len(line)):
+        line[i] = [
+            round(line[i][0], ndigits),
+            round(line[i][1], ndigits)
+        ]
+    line = tuple(line)
+
+    return line
