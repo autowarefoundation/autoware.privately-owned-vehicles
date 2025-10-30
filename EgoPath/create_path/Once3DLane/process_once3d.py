ca8841f026a3dea0282552f0827c1d28b49f0703
TranHuuNhatHuy <huy9515@gmail.com>
2025-10-30 10:38:19 +0900

    add round line floats

--- EgoPath/create_path/Once3DLane/process_once3d.py
+++ EgoPath/create_path/Once3DLane/process_once3d.py
@@ -33,3 +33,24 @@ def normalizeCoords(
         ) 
         for x, y in line
     ]
+
+
+def round_line_floats(
+    line: Line, 
+    ndigits: int = 3
+):
+    """
+    Round the coordinates of a line to a 
+    specified number of decimal places.
+    Default is 3 decimal places.
+    """
+
+    line = list(line)
+    for i in range(len(line)):
+        line[i] = [
+            round(line[i][0], ndigits),
+            round(line[i][1], ndigits)
+        ]
+    line = tuple(line)
+
+    return line
