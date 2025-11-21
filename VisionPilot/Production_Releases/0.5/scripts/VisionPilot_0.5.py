851d9fe934546d4587982803c0493787c714f3d0
TranHuuNhatHuy <huy9515@gmail.com>
2025-11-21 22:14:20 +0900

    tasks defined clear

--- VisionPilot/Production_Releases/0.5/scripts/VisionPilot_0.5.py
+++ VisionPilot/Production_Releases/0.5/scripts/VisionPilot_0.5.py
@@ -3,6 +3,8 @@ import cv2
 def main():
 
     video_filepath = "/mnt/Storage/Daihatsu/video_frames.avi"
+
+    # Read homography (should be computed once with findHomography)
     
     cap = cv2.VideoCapture(video_filepath)
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -18,17 +20,23 @@ def main():
         if not ret:
             break
 
-        # Crop + rescale: 2880 × 1860 ---> 2880 x 1440 ---> 640 x 320
+        # Crop: 2880 × 1860 ---> 2880 x 1440
+
+        # Rescale---> 640 x 320
 
         # Run inference
 
-        # Show raw binary mask
+        # Show raw binary mask (must be normalized so we can use homography)
 
-        # Convert raw binary mask to BEV
+        # Convert raw binary mask to BEV using homography
 
         # Process BEV to extract lane points
 
+        # Show BEV masks (debugging purpose)
+
         # Process lane points to get curve parameters of the road (lane offset, yaw angle, curvature)
+
+        # Show BEV vis with the curve parameters and sliding windows and basically everything that helps us debug
 
     cap.release()
 
