d3956dfa3ae61deb8cc05c2071ca61751e1bc444
TranHuuNhatHuy <huy9515@gmail.com>
2025-11-26 22:52:31 +0900

    master update func - initializations

--- VisionPilot/Production_Releases/0.5/src/lane_filtering/lane_filter.cpp
+++ VisionPilot/Production_Releases/0.5/src/lane_filtering/lane_filter.cpp
@@ -1,4 +1,5 @@
 #include "lane_filtering/lane_filter.hpp"
+#include "inference/onnxruntime_engine.hpp"
 #include <iostream>
 #include <cmath>
 #include <algorithm>
@@ -12,3 +13,28 @@ void LaneFilter::reset() {
     prev_left_fit.valid = false;
     prev_right_fit.valid = false;
 }
+
+// Master update function
+LaneSegmentation LaneFilter::update(const LaneSegmentation& raw_input) {
+    LaneSegmentation clean_output;
+    clean_output.width = raw_input.width;   // 160
+    clean_output.height = raw_input.height; // 80
+    
+    // Initialize blank masks
+    clean_output.ego_left = cv::Mat::zeros(
+        clean_output.height, 
+        clean_output.width, 
+        CV_32FC1
+    );
+    clean_output.ego_right = cv::Mat::zeros(
+        clean_output.height, 
+        clean_output.width, 
+        CV_32FC1
+    );
+    clean_output.other_lanes = cv::Mat::zeros(
+        clean_output.height, 
+        clean_output.width, 
+        CV_32FC1
+    );
+
+    
