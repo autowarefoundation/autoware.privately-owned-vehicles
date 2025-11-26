3a41fd376ce2f22f1fdfa7a740f3d9f65b66fd85
TranHuuNhatHuy <huy9515@gmail.com>
2025-11-26 22:51:13 +0900

    defining first lanefilter declarations

--- VisionPilot/Production_Releases/0.5/src/lane_filtering/lane_filter.cpp
+++ VisionPilot/Production_Releases/0.5/src/lane_filtering/lane_filter.cpp
@@ -2,3 +2,13 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+
+namespace autoware_pov::vision::autosteer
+{
+
+LaneFilter::LaneFilter(float smoothing_factor) : alpha(smoothing_factor) {}
+
+void LaneFilter::reset() {
+    prev_left_fit.valid = false;
+    prev_right_fit.valid = false;
+}
