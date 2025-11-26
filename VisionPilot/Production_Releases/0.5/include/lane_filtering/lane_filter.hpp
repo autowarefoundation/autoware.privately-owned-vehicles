30f4fb6831a8088b19529eacec635cc2117c89ae
TranHuuNhatHuy <huy9515@gmail.com>
2025-11-26 20:45:48 +0900

    [hpp] class LaneFilter public implementation

--- VisionPilot/Production_Releases/0.5/include/lane_filtering/lane_filter.hpp
+++ VisionPilot/Production_Releases/0.5/include/lane_filtering/lane_filter.hpp
@@ -7,10 +7,22 @@ namespace autoware_pov::vision::autosteer
 {
 
 /**
-    * @brief Struct to hold lane polynomial fitting results
-    */
+ * @brief Struct to hold lane polynomial fitting results
+ */
 struct LanePolyFit {
     std::vector<double> coeffs;  // Cubic polyfit coeffs [a, b, c, d] for ax^3 + bx^2 + cx + d
     bool valid;                  // Whether the lane fit is valid
 };
+
+class LaneFilter {
+public:
+    explicit LaneFilter(float smoothing_factor = 0.5f);
+
+    /**
+        * @brief Main processng func
+        * ROI starting points => Sliding window => Polyfit => Render clean masks
+    */
+    LaneSegmentation update(const LaneSegmentation& raw_input);
+
+    void reset();
 
