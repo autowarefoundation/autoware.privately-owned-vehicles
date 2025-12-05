512e8e87a8da35b48209a45dd42ec4ba5ecd19f2
TranHuuNhatHuy <huy9515@gmail.com>
2025-12-05 20:16:00 +0900

    container for curve params both views

--- VisionPilot/Production_Releases/0.5/include/lane_tracking/lane_tracking.hpp
+++ VisionPilot/Production_Releases/0.5/include/lane_tracking/lane_tracking.hpp
@@ -2,3 +2,25 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 #include <array>
+
+namespace autoware_pov::vision::autosteer
+{
+
+/**
+ * @brief Container for curve params both views
+ * (for statistics and debugging later)
+ */
+struct DualViewMetrics {
+
+    // Original perspective curve params
+    double pers_lane_offset = 0.0;
+    double pers_yaw_offset = 0.0;
+    double pers_curvature = 0.0;
+
+    // BEV perspective curve params
+    double bev_lane_offset = 0.0;
+    double bev_yaw_offset = 0.0;
+    double bev_curvature = 0.0;
+};
+
+}
