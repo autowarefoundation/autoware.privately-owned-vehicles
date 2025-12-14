b27bc80da1315d6cbe0f6b4545f76bdc1a112238
TranHuuNhatHuy <huy9515@gmail.com>
2025-12-14 20:49:54 +0900

    define libs in header

---
+++ VisionPilot/Production_Releases/0.5/include/drivers/can_interface.hpp
@@ -0,0 +1,10 @@
+#ifndef AUTOWARE_POV_DRIVERS_CAN_INTERFACE_HPP_
+#define AUTOWARE_POV_DRIVERS_CAN_INTERFACE_HPP_
+
+#include <string>
+#include <optional>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <limits>
+#include <chrono>
