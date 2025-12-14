ac48e61bc7829efd97594c2bc36e8789f4f21684
TranHuuNhatHuy <huy9515@gmail.com>
2025-12-14 21:10:49 +0900

    linux SocketCAN headers

--- VisionPilot/Production_Releases/0.5/src/drivers/can_interface.cpp
+++ VisionPilot/Production_Releases/0.5/src/drivers/can_interface.cpp
@@ -4,3 +4,13 @@
 #include <cstring>
 #include <sstream>
 #include <iomanip>
+
+// Linux SocketCAN headers
+#include <unistd.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <fcntl.h>
+
