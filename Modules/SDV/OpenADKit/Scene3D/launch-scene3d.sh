a3692f07de6cee034bc41925c6bae1abab2fb120
Oguz Ozturk <oguzkaganozt@gmail.com>
2026-01-16 14:11:00 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- Modules/SDV/OpenADKit/Scene3D/launch-scene3d.sh
+++ Modules/SDV/OpenADKit/Scene3D/launch-scene3d.sh
@@ -4,6 +4,6 @@
 docker run -it --rm \
     -p 6080:6080 \
     -v "$PWD"/model-weights:/autoware/model-weights \
-    -v "$PWD"/test:/autoware/test \
+    -v "$PWD"/../Test:/autoware/test \
     ghcr.io/autowarefoundation/visionpilot:latest \
     python3 /autoware/Models/visualizations/Scene3D/video_visualization.py -v -p /autoware/model-weights/scene3d.pth -i /autoware/test/traffic-driving.mp4 -o /autoware/test/output_scene3d.avi
