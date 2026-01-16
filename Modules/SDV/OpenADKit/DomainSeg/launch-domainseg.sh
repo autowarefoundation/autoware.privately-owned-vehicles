13e747b7317ad5b3ce0b662c6882163c1613f3af
Oguz Ozturk <oguzkaganozt@gmail.com>
2026-01-16 13:20:49 +0300

    .
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- Modules/SDV/OpenADKit/DomainSeg/launch-domainseg.sh
+++ Modules/SDV/OpenADKit/DomainSeg/launch-domainseg.sh
@@ -6,5 +6,5 @@ docker run -it --rm \
     -p 6080:6080 \
     -v "$PWD"/model-weights:/autoware/model-weights \
     -v "$PWD"/../Test:/autoware/test \
-    visionpilot:latest \
+    ghcr.io/autowarefoundation/visionpilot:latest \
     python3 /autoware/Models/visualizations/DomainSeg/video_visualization.py -v -p /autoware/model-weights/domainseg.pth -i /autoware/test/traffic-driving.mp4 -o /autoware/test/output_domainseg.avi
--- Modules/SDV/OpenADKit/Scene3D/launch-scene3d.sh
+++ Modules/SDV/OpenADKit/Scene3D/launch-scene3d.sh
@@ -8,5 +8,5 @@ docker run -it --rm \
     -p 6080:6080 \
     -v "$PWD"/model-weights:/autoware/model-weights \
     -v "$PWD"/test:/autoware/test \
-    visionpilot:latest \
+    ghcr.io/autowarefoundation/visionpilot:latest \
     python3 /autoware/Models/visualizations/Scene3D/video_visualization.py -v -p /autoware/model-weights/scene3d.pth -i /autoware/test/traffic-driving.mp4 -o /autoware/test/output_scene3d.avi
